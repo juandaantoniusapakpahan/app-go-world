508af03a847afcaed0c5822fec6b6383bd53de59
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-04-20 11:56:03 +0700

    app go and module-wolrd

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/juandaantoniusapakpahan/app-go-world
+
+go 1.20
+
+require github.com/juandaantoniusapakpahan/module-go-world v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	world_go "github.com/juandaantoniusapakpahan/module-go-world"
+)
+
+func main() {
+	fmt.Println(world_go.World())
+}
